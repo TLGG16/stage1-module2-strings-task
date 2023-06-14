0677bde5f395f384d99b4d0f00c6af4237bd1a1e
TLGG16 <TLGG16@yandex.ru>
2023-06-14 22:53:47 +0300

    kekw

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,25 +1,37 @@
 package com.epam.mjc;
 
-public class MethodParser {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
-    /**
-     * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
-     * signatureString is a java-like method signature with following parts:
-     *      1. access modifier - optional, followed by space: ' '
-     *      2. return type - followed by space: ' '
-     *      3. method name
-     *      4. arguments - surrounded with braces: '()' and separated by commas: ','
-     * Each argument consists of argument type and argument name, separated by space: ' '.
-     * Examples:
-     *      accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
-     *      private void log(String value)
-     *      Vector3 distort(int x, int y, int z, float magnitude)
-     *      public DateTime getCurrentDateTime()
-     *
-     * @param signatureString source string to parse
-     * @return {@link MethodSignature} object filled with parsed values from source string
-     */
+public class MethodParser {
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        String arg = signatureString.substring(signatureString.indexOf("(") + 1, signatureString.lastIndexOf(")"));
+        String[] list = arg.replace(",", "").split(" ");
+        List<MethodSignature.Argument> arguments = new ArrayList<>();
+        if (list.length > 1) {
+            for (int i = 0; i < list.length; i += 2) {
+                arguments.add(new MethodSignature.Argument(list[i], list[i + 1]));
+            }
+        }
+
+        String[] signature = signatureString.replace("(", " ").replace(")", " ").split(" ");
+        String modifier = "", type, name;
+        if (signature[0].equals("public") || signature[0].equals("private") ||  signature[0].equals("protected")) {
+            modifier = signature[0];
+            type = signature[1];
+            name = signature[2];
+        } else {
+            type = signature[0];
+            name = signature[1];
+        }
+
+        MethodSignature obj = new MethodSignature(name, arguments);
+        if (!modifier.equals("")) {
+            obj.setAccessModifier(modifier);
+        }
+        obj.setReturnType(type);
+        return obj;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,5 +1,6 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -12,7 +13,31 @@ public class StringSplitter {
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
-    public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
-    }
+        public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
+            List<String> list = new ArrayList<>();
+            int start = 0, end;
+
+            while (start < source.length()) {
+                end = source.length();
+                for (String s : delimiters) {
+                    int index = source.indexOf(s, start);
+                    if (index != -1 && index < end) {
+                        end = index;
+                    }
+                }
+
+                if (end != source.length()) {
+                    if(!source.substring(start, end).equals("")) {
+                        list.add(source.substring(start, end));
+                    }
+                    start = end + 1;
+                } else {
+                    if(!source.substring(start).equals("")) {
+                        list.add(source.substring(start));
+                    }
+                    break;
+                }
+            }
+            return list;
+        }
 }
